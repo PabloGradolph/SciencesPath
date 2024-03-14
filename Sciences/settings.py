ab525bb9687ce1d183165d96886734afd3b50fa7
PabloGradolph <pablo.gradolph@gmail.com>
2024-03-14 12:55:33 +0100

    Email settings

--- Sciences/settings.py
+++ Sciences/settings.py
@@ -16,6 +16,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.19']
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'pablo.gradolph@gmail.com'
+EMAIL_HOST_USER = 'pablo.gradolph@gmail.com'
+EMAIL_HOST_PASSWORD = 'uyps irqd xnif powt'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+PASSWORD_RESET_TIMEOUT = 14400
 
 # Application definition
 
